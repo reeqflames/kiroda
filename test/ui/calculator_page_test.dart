cb030f947cd4e2a0eb2c4830d6e1063a11024c01
reeqflames <perodua8@gmail.com>
2026-09-21 12:30:27 +0800

    test: isolate local storage in UI smoke tests

--- test/ui/calculator_page_test.dart
+++ test/ui/calculator_page_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:kiroda/main.dart';
 
 void main() {
+  setUp(() => SharedPreferences.setMockInitialValues({}));
   testWidgets('core calculator journey renders', (tester) async {
     await tester.pumpWidget(const KirodaApp());
     expect(find.text('Kira sebelum pandu.'), findsOneWidget);
